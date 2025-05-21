c6b094a05cda0d6997b0eb1be884ff72079d7db6
Chad Baker <chadster5200@gmail.com>
2025-05-20 23:30:52 -0700

    Added getSingles function

--- src/main/scala/sudoku/SudokuProcessor.scala
+++ src/main/scala/sudoku/SudokuProcessor.scala
@@ -3,8 +3,13 @@ package sudoku
 import chisel3._
 import chisel3.util._
 
-class SudokuProcessor() extends Module {
-    // Find candidates and singles
+class SudokuProcessor(gridSize: Int) extends Module {
+    val io = IO(new Bundle {
+        val inGrid = Input(Vec(gridSize*gridSize, UInt(gridSize.W)))
+        val outGrid = Output(Vec(gridSize*gridSize, UInt(gridSize.W)))
+    })
+
+    // Set Condidate Masks
     def setCandidates(cells: Vec[UInt]): (Vec[UInt], Vec[UInt], Vec[UInt]) = {
         // 27 9-bit vectors
         val rowMask = WireInit(VecInit(Seq.fill(9)(0.U(gridSize.W))))
@@ -20,11 +25,32 @@ class SudokuProcessor() extends Module {
                 // Col
                 colMask(col) := colMask(col) | cell
                 // Sub
-                val boxIdx = ((row / 3) * 3) + (col / 3)
-                subMask(boxIdx) := subMask(boxIdx) | cell
+                val boxInd = row + (col / 3)
+                subMask(boxInd) := subMask(boxInd) | cell
             }
         }
 
         (rowMask, colMask, subMask)
     }
+    
+    // Update Candidates and find Singles
+    def getSingles(cells: Vec[UInt], rowMask: Vec[Uint], colMask: Vec[Uint], subMask: Vec[Uint]): Vec[UInt] = {
+        val updatedCells = WireInit(VecInit(Seq.fill(9*9)(0.U(gridSize.W))))
+
+        for (row <- 0 until 9) {
+            for (col <- 0 until 9) {
+                val boxInd = row + (col / 3)
+                val cell = io.inGrid((row*9) + col)
+                val finalMask = ~(rowMask(row) | colMask(col) | subMask(boxInd))
+
+                updatedCells((row*9) + col) := cell & finalMask
+            }
+        }
+
+        updatedCells
+    }
+
+
+    val (rowMask, colMask, subMask) = setCandidates()
+    io.outGrid := getSingles(io.inGrid, rowMask, colMask, subMask)
 }
