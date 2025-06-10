2ba9d755fdf0203b3d1cfeca0ce274ee1b4f9244
Chad Baker <chadster5200@gmail.com>
2025-06-09 19:16:54 -0700

    took out internal grid and hidden singles state

--- src/main/scala/sudoku/SudokuController.scala
+++ src/main/scala/sudoku/SudokuController.scala
@@ -8,51 +8,60 @@ class SudokuController() extends Module {
         val start = Input(Bool())
         val changed = Input(Bool())
         val solved = Input(Bool())
-        val inGrid = Input(Vec(9*9, UInt(9.W)))
         val loadGrid = Output(Bool())
         val done = Output(Bool())
         val mode = Output(UInt(2.W))
-        val cycleCount = Output(UInt())
-        val outGrid = Output(Vec(9*9, UInt(9.W)))
+        val cycleCount = Output(UInt(32.W))
     })
+    val grid = RegInit(VecInit(Seq.fill(9*9)(0.U(9.W))))
 
     object pState extends ChiselEnum {
         val Idle, Single, Hidden, DFS, Done = Value
     }
-
     val state = RegInit(pState.Idle)
-    
-    val grid = WireInit(VecInit(Seq.fill(9*9)(0.U(9.W))))
+
+    val done = RegInit(false.B)
+    val nextMode = RegInit(0.U(2.W))
+    val startCount = (state === pState.Single) || (state === pState.Hidden) || (state === pState.DFS)
+    val (cycles, _) = Counter(startCount, 16777216) // allows for 10 mil cycles
+    val load = state === pState.Idle
 
     switch (state) {
         is (pState.Idle) {
+            nextMode := 0.U
             when (io.start) {
+                nextMode := 1.U
                 state := pState.Single
             }
         }
         is (pState.Single) {
+            nextMode := 1.U
             when (io.solved) {
+                nextMode := 0.U
                 state := pState.Done
-            } .elsewhen (~io.changed & ~io.solved) {
-                state := pState.Hidden
-            }
-        }
-        is (pState.Hidden) {
-            when (io.solved) {
-                state := pState.Done
-            } .elsewhen (io.changed) {
-                state := pState.Single
-            } .otherwise {
+            } .elsewhen (~io.changed) { // & ~solved
+                nextMode := 2.U
                 state := pState.DFS
             }
         }
         is (pState.DFS) {
             when (io.solved) {
+                nextMode := 0.U
+                done := true.B
                 state := pState.Done
             }
+            
+            nextMode := 2.U
         }
-        // is (pState.Done) {
-
-        // }
+        is (pState.Done) {
+            nextMode := 0.U
+            done := true.B
+        }
     }
+
+    
+    io.loadGrid := load
+    io.done := done
+    io.mode := nextMode
+    io.cycleCount := cycles
 }
