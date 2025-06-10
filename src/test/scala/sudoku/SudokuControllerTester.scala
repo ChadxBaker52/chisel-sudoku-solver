ba4b26a0e2daf031d00d4f28bdf12a1978885550
Chad Baker <chadster5200@gmail.com>
2025-06-09 19:17:37 -0700

    fixed test

--- src/test/scala/sudoku/SudokuControllerTester.scala
+++ src/test/scala/sudoku/SudokuControllerTester.scala
@@ -5,56 +5,61 @@ import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 
 class SudokuControllerTester extends AnyFlatSpec with ChiselScalatestTester {
-  "SudokuController" should "Go through all states (Idle -> Single -> Hidden -> DFS -> Done)" in {
+  // def simController() = {
+
+  // }
+
+  behavior of "SudokuController" 
+  it should "Go through all states (Idle -> Single -> Hidden -> DFS -> Done)" in {
     test(new SudokuController()) { dut =>
 
-        // Idle
-        dut.io.start.poke(false.B)
-        dut.io.changed.poke(false.B)
-        dut.io.solved.poke(false.B)
+      // Idle
+      dut.io.start.poke(false.B)
+      dut.io.changed.poke(false.B)
+      dut.io.solved.poke(false.B)
 
-        dut.io.done.expect(false.B)
-        dut.io.mode.expect(0.U)
+      dut.io.done.expect(false.B)
+      dut.io.mode.expect(0.U)
+      dut.io.loadGrid.expect(true.B)
 
-        dut.clock.step(1)       // Next cycle
+      dut.clock.step(1)       // Next cycle
 
-        // Idle -> Single
-        dut.io.start.poke(true.B)
-        dut.io.changed.poke(false.B)
-        dut.io.solved.poke(false.B)
+      // Idle -> Single
+      dut.io.start.poke(true.B)
+      dut.io.changed.poke(false.B)
+      dut.io.solved.poke(false.B)
 
-        dut.io.done.expect(false.B)
-        dut.io.mode.expect(0.U)
+      dut.io.done.expect(false.B)
+      dut.io.mode.expect(0.U)
+      dut.io.loadGrid.expect(true.B)
 
-        dut.clock.step(1)       // Next cycle
+      dut.clock.step(1)       // Next cycle
 
-        // Single -> Hidden
-        dut.io.start.poke(false.B)
-        dut.io.changed.poke(false.B)
-        dut.io.solved.poke(false.B)
+      // Single -> DFS
+      dut.io.start.poke(false.B)
+      dut.io.changed.poke(false.B)
+      dut.io.solved.poke(false.B)
 
-        dut.io.done.expect(false.B)
-        dut.io.mode.expect(1.U)
+      dut.io.done.expect(false.B)
+      dut.io.mode.expect(1.U)
+      dut.io.loadGrid.expect(false.B)
 
-        dut.clock.step(1)       // Next cycle
+      dut.clock.step(1)       // Next cycle
 
-        // Hidden -> DFS
-        dut.io.start.poke(false.B)
-        dut.io.changed.poke(false.B)
-        dut.io.solved.poke(false.B)
+      // DFS -> Done
+      dut.io.start.poke(false.B)
+      dut.io.changed.poke(false.B)
+      dut.io.solved.poke(true.B)
 
-        dut.io.done.expect(false.B)
-        dut.io.mode.expect(2.U)
+      dut.io.done.expect(false.B)
+      dut.io.mode.expect(2.U)
+      dut.io.loadGrid.expect(false.B)
 
-        dut.clock.step(1)       // Next cycle
+      dut.clock.step(1)       // Next cycle
 
-        // DFS -> Done
-        dut.io.start.poke(false.B)
-        dut.io.changed.poke(false.B)
-        dut.io.solved.poke(false.B)
-
-        dut.io.done.expect(false.B)
-        dut.io.mode.expect(3.U)
+      dut.io.done.expect(true.B)
+      dut.io.mode.expect(2.U)
+      dut.io.loadGrid.expect(false.B)
     }
   }
 }
